f2fd8a6fcc8b9e1b236ce2edbcf45e3027205c08
Abdullah <79649608+Abdullah-117@users.noreply.github.com>
2021-08-09 12:31:14 +0300

    Add files via upload

---
+++ test3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace test3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = 1;
+            int second = 1;
+   
+            int input = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+
+            for (int i = 1; input > i; i++)
+            {
+                result = first + second;
+                first = second;
+                second = result;
+            }
+            Console.WriteLine(Math.Abs(result));
+        }
+    }
+}
